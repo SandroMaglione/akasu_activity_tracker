ade2395120f44d6778b0cf12b84838d212aaefeb
SandroMaglione <lass.maglio@gmail.com>
2024-01-26 05:38:05 +0100

    setup freezed models

---
+++ lib/models/activity_model.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'activity_model.freezed.dart';
+part 'activity_model.g.dart';
+
+@freezed
+class ActivityModel with _$ActivityModel {
+  const factory ActivityModel({
+    required String name,
+    required String emojy,
+  }) = _ActivityModel;
+
+  factory ActivityModel.fromJson(Map<String, Object?> json) =>
+      _$ActivityModelFromJson(json);
+}
