0de4ef43e54525d08a1f6b5b30e1fff8b655a56e
SandroMaglione <lass.maglio@gmail.com>
2024-01-26 04:59:01 +0100

    init project

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: const Placeholder(),
+    );
+  }
+}
