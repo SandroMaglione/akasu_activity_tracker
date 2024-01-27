b332259d13a3dc057c7e56ee5ee09147ff0aeef6
SandroMaglione <lass.maglio@gmail.com>
2024-01-27 06:16:24 +0100

    refactor stream no signal

--- lib/routes/home/home_screen.dart
+++ lib/routes/home/home_screen.dart
@@ -1,16 +1,10 @@
 import 'package:akasu_activity_tracker/database.dart';
 import 'package:akasu_activity_tracker/get_it.dart';
-import 'package:akasu_activity_tracker/models/activity_model.dart';
 import 'package:akasu_activity_tracker/routes/home/activity_card.dart';
 import 'package:akasu_activity_tracker/routes/home/insert_activity_form.dart';
 import 'package:akasu_activity_tracker/routes/home/stream_listener.dart';
-import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:flutter/material.dart';
 import 'package:signals/signals_flutter.dart';
-
-final allActivitiesSignal = streamSignal(
-  () => getIt.get<Database>().watchActivities,
-);
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -25,26 +19,16 @@ class HomeScreen extends StatelessWidget {
             const InsertActivityForm(),
             Watch(
               (context) => StreamListener(
-                allActivitiesSignal.toStream(),
-                builder: (context, data) => switch (data) {
-                  AsyncLoading<IList<ActivityModel>>() =>
-                    const Text("Loading..."),
-                  AsyncError<IList<ActivityModel>>(error: final error) =>
-                    Text("Steam error: $error"),
-                  AsyncData<IList<ActivityModel>>(value: final value) =>
-                    switch (value) {
-                      null => const Text("Missing data"),
-                      final list => Column(
-                          children: list
-                              .map(
-                                (activity) => ActivityCard(
-                                  activity: activity,
-                                ),
-                              )
-                              .toList(),
-                        )
-                    },
-                },
+                getIt.get<Database>().watchActivities,
+                builder: (context, data) => Column(
+                  children: data
+                      .map(
+                        (activity) => ActivityCard(
+                          activity: activity,
+                        ),
+                      )
+                      .toList(),
+                ),
               ),
             ),
           ],
